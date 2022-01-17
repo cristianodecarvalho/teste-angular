6f7f8b3d2418f22f6ff98a3138659a008961b43e
Cristiano de Carvalho <cristianfilho18@gmail.com>
2022-01-17 12:20:14 -0300

    Adicionando precisão no teste

--- src/app/shared/services/uniqued-id/unique-id.service.spec.ts
+++ src/app/shared/services/uniqued-id/unique-id.service.spec.ts
@@ -4,7 +4,7 @@ describe(UniqueIdService.name, () => {
     it(`#${UniqueIdService.prototype.generateUniqueIdWithPrefix.name} should generate id when called with prefix`, () => {
         const service = new UniqueIdService();
         const id = service.generateUniqueIdWithPrefix('app');
-        expect(id).toContain('app-')
+        expect(id.startsWith('app-')).toBeTrue();
     });
 
 });
