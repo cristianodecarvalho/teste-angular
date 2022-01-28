770b63866198dde21b3fd832d05c40a8500ed8bd
Cristiano de Carvalho <cristianfilho18@gmail.com>
2022-01-28 16:56:11 -0300

    Testando funções assíncronas

--- src/app/shared/components/like-widget/like-widget.component.spec.ts
+++ src/app/shared/components/like-widget/like-widget.component.spec.ts
@@ -6,6 +6,7 @@ import { LikeWidgetModule } from "./like-widget.module";
 
 describe(LikeWidgetComponent.name, () => {
     let fixture: ComponentFixture<LikeWidgetComponent> = null;
+    let component: LikeWidgetComponent = null;
 
     beforeEach(async () => {
         await TestBed.configureTestingModule({
@@ -13,26 +14,31 @@ describe(LikeWidgetComponent.name, () => {
         }).compileComponents();
 
         fixture = TestBed.createComponent(LikeWidgetComponent);
+        component = fixture.componentInstance;
     });
 
     it('Should create component', () => {
-        const component = fixture.componentInstance;
         expect(component).toBeTruthy();
     });
 
     it('Should auto generate ID when id input property is missing', () => {
-        const component = fixture.componentInstance;
         fixture.detectChanges();
         expect(component.id).toBeTruthy();
     });
 
     it('Should NOT generate ID when id input property is present', () => {
-        const component = fixture.componentInstance;
         const someId = 'someId';
         component.id = someId;
         fixture.detectChanges();
         expect(component.id).toBe(someId);
     });
 
-
+    it(`#${LikeWidgetComponent.prototype.like.name} should trigger emission when called`, done => {
+        fixture.detectChanges();
+        component.liked.subscribe(() => {
+            expect(true).toBeTrue();
+            done();
+        })
+        component.like();
+    });
 })
